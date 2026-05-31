3990d0fd82a93246c16eab736c5e080aa346408e
mphomosia194 <mphomosia194@gmail.com>
2026-05-31 15:34:51 +0200

    Add task 3 private password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,22 @@
+class Password {
+  String _password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    RegExp upperCase = RegExp(r'[A-Z]');
+    RegExp lowerCase = RegExp(r'[a-z]');
+    RegExp number = RegExp(r'[0-9]');
+
+    return _password.length >= 8 &&
+        _password.length <= 16 &&
+        upperCase.hasMatch(_password) &&
+        lowerCase.hasMatch(_password) &&
+        number.hasMatch(_password);
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
