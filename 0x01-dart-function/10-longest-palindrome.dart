f4e0ea1d8c38010004d1c538c4dd89d260cfc120
mphomosia194 <mphomosia194@gmail.com>
2026-05-31 15:28:41 +0200

    Add task 10 longest palindrome

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,22 @@
+bool isPalindrome(String s) {
+  if (s.length < 3) {
+    return false;
+  }
+  return s == s.split('').reversed.join('');
+}
+
+String longestPalindrome(String s) {
+  String longest = '';
+
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 1; j <= s.length; j++) {
+      String sub = s.substring(i, j);
+
+      if (isPalindrome(sub) && sub.length > longest.length) {
+        longest = sub;
+      }
+    }
+  }
+
+  return longest.isEmpty ? 'none' : longest;
+}
